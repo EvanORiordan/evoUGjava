3e205c9a15ae6b8db339f7ce5e8b15ed3bc8ac43
Evan O'Riordan <46937193+EvanORiordan@users.noreply.github.com>
2023-02-23 16:53:03 +0000

    replace Runner4.java with SADG9.main()

--- src/Runner4.java
+++ src/Runner4.java
@@ -1,82 +1,82 @@
-import java.math.RoundingMode;
-import java.time.Duration;
-import java.time.Instant;
-
-/**
- * Program for running multiple instances of an experiment program. Supports SADG9.
- */
-public class Runner4 {
-    public static void main(String[] args) {
-        System.out.println("Executing "+Thread.currentThread().getStackTrace()[1].getClassName()+"."
-                +Thread.currentThread().getStackTrace()[1].getMethodName()+"()...");
-
-        System.out.println("Timestamp:" + java.time.Clock.systemUTC().instant());
-
-        // initialise variables in preparation for experimentation
-        int runs=10;
-        Player.setPrize(1.0);
-        Player.setLoners_payoff(Player.getPrize() * 0.2);
-        Player.setNeighbourhoodType("VN");
-        Player.getDf().setRoundingMode(RoundingMode.UP);
-        SADG9.rows=30;
-        SADG9.columns=30;
-        SADG9.N = SADG9.rows * SADG9.columns;
-        SADG9.max_gens=500;
-        SADG9.initial_num_abstainers = SADG9.N / 2;
-
-        System.out.println("Runs="+runs
-                + ", gens="+SADG9.max_gens
-                + ", l="+Player.getLoners_payoff()
-                + ", neighbourhood="+Player.getNeighbourhoodType()
-                + ", pop size="+SADG9.N
-                + ", init abstainers="+SADG9.initial_num_abstainers
-                +": ");
-
-        // stats representing experiment results
-        double mean_avg_p = 0;
-        double[] mean_avg_p_values = new double[runs];
-        double standard_deviation_mean_avg_p = 0;
-        int mean_abstainers = 0;
-        int[] mean_abstainers_values = new int[runs];
-        double standard_deviation_mean_abstainers = 0;
-
-        // run some experiments
-        System.out.println("Results of each run: ");
-        Instant start = Instant.now(); // start the stopwatch
-        for(int i=0;i<runs;i++){
-            SADG9 run = new SADG9();
-            run.start();
-            mean_avg_p += run.avg_p;
-            System.out.println("Avg p = " + run.avg_p);
-            mean_avg_p_values[i] = run.avg_p;
-            mean_abstainers += run.abstainers;
-            System.out.println("Abstainers remaining = " + run.abstainers + "\n");
-            mean_abstainers_values[i] = run.abstainers;
-        }
-        Instant finish = Instant.now(); // stop the stopwatch
-        long secondsElapsed = Duration.between(start, finish).toSeconds();
-        long minutesElapsed = Duration.between(start, finish).toMinutes();
-
-        // display stats
-        mean_avg_p /= runs;
-        mean_abstainers /= runs;
-        for(int i=0;i<runs;i++){
-            standard_deviation_mean_avg_p += Math.pow(mean_avg_p_values[i] - mean_avg_p, 2);
-            standard_deviation_mean_abstainers += Math.pow(mean_abstainers_values[i] - mean_abstainers, 2);
-        }
-        standard_deviation_mean_avg_p = Math.pow(standard_deviation_mean_avg_p / runs, 0.5);
-        standard_deviation_mean_abstainers = Math.pow(standard_deviation_mean_abstainers / runs, 0.5);
-
-        System.out.println("Final statistics:\n"
-                        + "Mean avg p="+Player.getDf().format(mean_avg_p)
-                        + ", standard deviation mean avg p="+Player.getDf().format(standard_deviation_mean_avg_p)
-                        + ", mean abstainers="+mean_abstainers
-                        + ", standard deviation mean abstainers="+Player.getDf().format(standard_deviation_mean_abstainers)
-        );
-
-        System.out.print("Time elapsed: "+minutesElapsed+" minutes, "+secondsElapsed%60+" seconds");
-
-        System.out.println("Timestamp:" + java.time.Clock.systemUTC().instant());
-
-    }
-}
+//import java.math.RoundingMode;
+//import java.time.Duration;
+//import java.time.Instant;
+//
+///**
+// * Program for running multiple instances of an experiment program. Supports SADG9.
+// * Invalidated by SADG9.main().
+// */
+//public class Runner4 {
+//    public static void main(String[] args) {
+//        System.out.println("Executing "+Thread.currentThread().getStackTrace()[1].getClassName()+"."
+//                +Thread.currentThread().getStackTrace()[1].getMethodName()+"()...");
+//
+//        System.out.println("Timestamp:" + java.time.Clock.systemUTC().instant());
+//
+//        // initialise variables in preparation for experimentation
+//        int runs=5000;
+//        Player.setPrize(1.0);
+//        Player.setLoners_payoff(Player.getPrize() * 0.2);
+//        Player.setNeighbourhoodType("VN");
+//        Player.getDf().setRoundingMode(RoundingMode.UP);
+//        SADG9.rows=30;
+//        SADG9.columns=30;
+//        SADG9.N = SADG9.rows * SADG9.columns;
+//        SADG9.max_gens=10000;
+//        SADG9.initial_num_abstainers = SADG9.N / 20;
+//
+//        System.out.println("Runs="+runs
+//                + ", gens="+SADG9.max_gens
+//                + ", l="+Player.getLoners_payoff()
+//                + ", neighbourhood="+Player.getNeighbourhoodType()
+//                + ", pop size="+SADG9.N
+//                + ", init abstainers="+SADG9.initial_num_abstainers
+//                +": ");
+//
+//        // stats representing experiment results
+//        double mean_avg_p = 0;
+//        double[] mean_avg_p_values = new double[runs];
+//        double standard_deviation_mean_avg_p = 0;
+//        int mean_abstainers = 0;
+//        int[] mean_abstainers_values = new int[runs];
+//        double standard_deviation_mean_abstainers = 0;
+//
+//        // run some experiments
+////        System.out.println("Results of each run: ");
+//        Instant start = Instant.now(); // start the stopwatch
+//        for(int i=0;i<runs;i++){
+//            SADG9 run = new SADG9();
+//            run.start();
+//            mean_avg_p += run.avg_p;
+////            System.out.println("Avg p = " + run.avg_p);
+//            mean_avg_p_values[i] = run.avg_p;
+//            mean_abstainers += run.abstainers;
+////            System.out.println("Abstainers remaining = " + run.abstainers + "\n");
+//            mean_abstainers_values[i] = run.abstainers;
+//        }
+//        Instant finish = Instant.now(); // stop the stopwatch
+//        long secondsElapsed = Duration.between(start, finish).toSeconds();
+//        long minutesElapsed = Duration.between(start, finish).toMinutes();
+//
+//        // display stats
+//        mean_avg_p /= runs;
+//        mean_abstainers /= runs;
+//        for(int i=0;i<runs;i++){
+//            standard_deviation_mean_avg_p += Math.pow(mean_avg_p_values[i] - mean_avg_p, 2);
+//            standard_deviation_mean_abstainers += Math.pow(mean_abstainers_values[i] - mean_abstainers, 2);
+//        }
+//        standard_deviation_mean_avg_p = Math.pow(standard_deviation_mean_avg_p / runs, 0.5);
+//        standard_deviation_mean_abstainers = Math.pow(standard_deviation_mean_abstainers / runs, 0.5);
+//
+//        System.out.println("Mean avg p="+Player.getDf().format(mean_avg_p)
+//                        + ", standard deviation mean avg p="+Player.getDf().format(standard_deviation_mean_avg_p)
+//                        + ", mean abstainers="+mean_abstainers
+//                        + ", standard deviation mean abstainers="+Player.getDf().format(standard_deviation_mean_abstainers)
+//        );
+//
+//        System.out.print("Time elapsed: "+minutesElapsed+" minutes, "+secondsElapsed%60+" seconds");
+//
+//        System.out.println("Timestamp:" + java.time.Clock.systemUTC().instant());
+//
+//    }
+//}
--- src/SADG9.java
+++ src/SADG9.java
@@ -1,3 +1,7 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,16 +15,19 @@ import java.util.concurrent.ThreadLocalRandom;
  * remain unchanged.
  *
  * This SADG version does not keep track of the highest or lowest value of p in the pop.
+ * This SADG version replaces its complementary runner class (Runner4.java) with SADG9.main().
+ * This SADG version reintroduces reset() and getStats().
  */
 public class SADG9 extends Thread{
-    static int rows;
+    static int rows; // all instances of a class share the same value of a static attribute
     static int columns;
     static int N;
     static int max_gens;
     static int initial_num_abstainers;
-    ArrayList<ArrayList<Player>> grid = new ArrayList<>();
+    ArrayList<ArrayList<Player>> grid = new ArrayList<>(); // non-static attribute values may vary per instance
     double avg_p=0;
     int abstainers = 0;
+    static DecimalFormat df = Player.getDf();
 
     public void start(){
         // generate fixed number of unique random abstainer positions;
@@ -35,9 +42,9 @@ public class SADG9 extends Thread{
         for(int i=0;i<rows;i++){
             ArrayList<Player> row = new ArrayList<>();
             for(int j=0;j<columns;j++){
-                boolean abstainer = false; // by default, a player is a non-abstainer
+                boolean abstainer = false; // by default, a player initialises as a non-abstainer
                 for(Integer abstainer_position: abstainer_positions){
-                    if(pop_position == abstainer_position){
+                    if(pop_position == abstainer_position){ // if true, this player is an abstainer
                         abstainer=true;
                         break;
                     }
@@ -64,14 +71,15 @@ public class SADG9 extends Thread{
                 }
             }
 
+            displayScreenshotOfPop();
+
             // evolution
             for(ArrayList<Player> row: grid){
                 for(Player player: row){ // the player here is the evolving player
 
-                    // the player throws their own imitation score into the mix by adding a 1.0 to the end of
-                    // the array. when j is equal to the neighbourhood size, this indicates that the current
-                    // imitation score in consideration is that of the evolving player therefore there are no
-                    // further neighbours to consider as parents therefore no evolution occurs here.
+                    // by 1.0 being added to the imitation score total, the player itself is effectively marked as a
+                    // potential parent. if the random double is too big for the other neighbours to be selected then
+                    // the player is effectively the parent therefore no change actually occurs.
                     ArrayList<Player> neighbourhood = player.getNeighbourhood();
                     double[] imitation_scores = new double[neighbourhood.size() + 1];
                     double total_imitation_score = 0;
@@ -80,46 +88,104 @@ public class SADG9 extends Thread{
                         imitation_scores[i] = Math.exp(neighbourhood.get(i).getAverage_score() - player_avg_score);
                         total_imitation_score += imitation_scores[i];
                     }
-                    imitation_scores[neighbourhood.size()] = 1.0;
                     total_imitation_score += 1.0;
                     double imitation_score_tally = 0;
                     double random_double_to_beat = ThreadLocalRandom.current().nextDouble();
-                    for(int j=0;j<imitation_scores.length;j++){
-                        if(j != neighbourhood.size()){
-                            imitation_score_tally += imitation_scores[j];
-                            if(random_double_to_beat < imitation_score_tally / total_imitation_score) {
-                                player.copyStrategy(neighbourhood.get(j));
-                                break;
-                            }
+                    for(int j=0;j<neighbourhood.size();j++){
+                        imitation_score_tally += imitation_scores[j];
+                        if(random_double_to_beat < imitation_score_tally / total_imitation_score) {
+                            player.copyStrategy(neighbourhood.get(j));
+                            break;
                         }
                     }
                 }
             }
 
-            // reset the players' scores, GPTG, old p value and old abstainer values.
-            for(ArrayList<Player> row: grid){
-                for(Player player: row){
-                    player.setScore(0);
-                    player.setGamesPlayedThisGen(0);
-                    player.setOld_p(player.getP());
-                    player.setOldAbstainer(player.getAbstainer());
-                }
-            }
-
-            double current_abstainers = 0;
-            for(ArrayList<Player> row: grid){
-                for(Player player: row){
-                    if(player.getAbstainer()){
-                        current_abstainers++;
-                    }
-                }
-            }
-            System.out.println(current_abstainers);
-
+            // end of generation.
+            reset();
             gen++;
         }
 
-        // gets stats
+        getStats();
+    }
+
+    // run experiments in this program's SADG environment
+    public static void main(String[] args) {
+        // display the name of the current program that is running
+        System.out.println("Executing "+Thread.currentThread().getStackTrace()[1].getClassName()+"."
+                +Thread.currentThread().getStackTrace()[1].getMethodName()+"()...");
+
+        // display initial timestamp
+        System.out.println("Timestamp:" + java.time.Clock.systemUTC().instant());
+
+        // variables that define the characteristics/settings of the experiment
+        int runs=1;
+        Player.setPrize(1.0);
+        Player.setLoners_payoff(Player.getPrize() * 0.2);
+        Player.setNeighbourhoodType("VN");
+        df.setRoundingMode(RoundingMode.UP);
+        SADG9.rows = 10;
+        SADG9.columns = 10;
+        SADG9.N = SADG9.rows * SADG9.columns;
+        SADG9.max_gens = 100;
+        SADG9.initial_num_abstainers = SADG9.N / 2;
+
+        // display settings
+        System.out.println("Runs="+runs
+                + ", gens="+SADG9.max_gens
+                + ", l="+Player.getLoners_payoff()
+                + ", neighbourhood="+Player.getNeighbourhoodType()
+                + ", pop size="+SADG9.N
+                + ", init abstainers="+SADG9.initial_num_abstainers
+                +": ");
+
+        // variables for storing experiment results
+        double avg_p = 0; // technically, this is the mean average value of p across the experiment's runs
+        double[] avg_p_values = new double[runs]; // average p values obtained from the runs
+        double sd_avg_p = 0; // the standard deviation of the group of avg p values
+        int avg_abstainers = 0;
+        int[] avg_abstainers_values = new int[runs];
+        double sd_avg_abstainers = 0;
+
+        // run the experiment
+        Instant start = Instant.now(); // start the stopwatch
+        for(int i=0;i<runs;i++){
+            SADG9 run = new SADG9();
+            run.start();
+            avg_p += run.avg_p;
+            avg_p_values[i] = run.avg_p;
+            avg_abstainers += run.abstainers;
+            avg_abstainers_values[i] = run.abstainers;
+        }
+        Instant finish = Instant.now(); // stop the stopwatch
+
+        // determine experiment results
+        avg_p /= runs;
+        avg_abstainers /= runs;
+        for(int i=0;i<runs;i++){
+            sd_avg_p += Math.pow(avg_p_values[i] - avg_p, 2);
+            sd_avg_abstainers += Math.pow(avg_abstainers_values[i] - avg_abstainers, 2);
+        }
+        sd_avg_p = Math.pow(sd_avg_p / runs, 0.5);
+        sd_avg_abstainers = Math.pow(sd_avg_abstainers / runs, 0.5);
+
+        // display experiment results
+        System.out.println("avg p="+df.format(avg_p)
+                + ", standard deviation of avg p="+df.format(sd_avg_p)
+                + ", avg abstainers="+avg_abstainers
+                + ", standard deviation of avg abstainers="+df.format(sd_avg_abstainers)
+        );
+
+        // display the time taken by the experiment
+        long secondsElapsed = Duration.between(start, finish).toSeconds();
+        long minutesElapsed = Duration.between(start, finish).toMinutes();
+        System.out.print("Time elapsed: "+minutesElapsed+" minutes, "+secondsElapsed%60+" seconds");
+        System.out.println("Timestamp:" + java.time.Clock.systemUTC().instant());
+    }
+
+
+    // gets the average p value and the number of abstainers left.
+    public void getStats(){
         for(ArrayList<Player> row: grid){
             for(Player player: row){
                 avg_p+=player.getP();
@@ -129,5 +195,43 @@ public class SADG9 extends Thread{
             }
         }
         avg_p /= N;
+    }
+
+    // reset the players' scores, GPTG, old p value and old abstainer values.
+    public void reset(){
+        for(ArrayList<Player> row: grid){
+            for(Player player: row){
+                player.setScore(0);
+                player.setGamesPlayedThisGen(0);
+                player.setOld_p(player.getP());
+                player.setOldAbstainer(player.getAbstainer());
+            }
+        }
+    }
+
+    // displays a screenshot of some cluster of the population
+    public void displayScreenshotOfPop(){
+
+        // obtain a 3x3 cluster of the pop
+        ArrayList<Player> cluster = new ArrayList<>();
+        cluster.add(grid.get(0).get(0));
+        cluster.add(grid.get(0).get(1));
+        cluster.add(grid.get(0).get(2));
+        cluster.add(grid.get(1).get(0));
+        cluster.add(grid.get(1).get(1)); // central node of cluster
+        cluster.add(grid.get(1).get(2));
+        cluster.add(grid.get(2).get(0));
+        cluster.add(grid.get(2).get(1));
+        cluster.add(grid.get(2).get(2));
+
+        System.out.print(df.format(cluster.get(0).getP()) + cluster.get(0).getAbstainer() + "\t");
+        System.out.print(df.format(cluster.get(1).getP()) + cluster.get(1).getAbstainer() + "\t");
+        System.out.println(df.format(cluster.get(2).getP()) + cluster.get(2).getAbstainer());
+        System.out.print(df.format(cluster.get(3).getP()) + cluster.get(3).getAbstainer() + "\t");
+        System.out.print(df.format(cluster.get(4).getP()) + cluster.get(4).getAbstainer() + "\t");
+        System.out.println(df.format(cluster.get(5).getP()) + cluster.get(5).getAbstainer());
+        System.out.print(df.format(cluster.get(6).getP()) + cluster.get(6).getAbstainer() + "\t");
+        System.out.print(df.format(cluster.get(7).getP()) + cluster.get(7).getAbstainer() + "\t");
+        System.out.println(df.format(cluster.get(8).getP()) + cluster.get(8).getAbstainer()+"\n");
     }
 }
